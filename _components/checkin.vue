e65058b0fa186e3775cec118e8abc5463b9fa20f
imagina <msolano2505@gmail.com>
2023-04-10 10:38:39 -0500

    +Update: Implement the apiResponse plugin in the catch calls

--- _components/checkin.vue
+++ _components/checkin.vue
@@ -182,8 +182,10 @@ export default {
           this.loading = false
           resolve(response.data)
         }).catch(error => {
-          this.loading = false
-          reject(error)
+          this.$apiResponse.handleError(error, () => {
+            this.loading = false
+            reject(error)
+          })
         })
       })
     },
